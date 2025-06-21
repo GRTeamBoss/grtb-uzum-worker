4851481d9dba6393a58ead529a8509f0a5631c37
GRTeamBoss <rabbitwow97@gmail.com>
2025-06-21 09:02:02 +0500

    fix

--- overwrite.py
+++ overwrite.py
@@ -3,7 +3,10 @@ import os
 def main() -> None:
   with open("./node_modules/@nuxt/ui-pro/dist/module.mjs", "w+", encoding="utf-8") as file:
     content = file.read()
+    print(content)
     arr = content.splitlines()
+    print("#")
+    print(arr)
     arr[4] = "import { a as version, n as name, i as icons, b as addTemplates, t as theme } from './shared/ui-pro.B6uD94pG.mjs';"
     arr[119] = "      return;"
     file.writelines(arr)
@@ -11,7 +14,10 @@ def main() -> None:
 
   with open("./node_modules/@nuxt/ui-pro/dist/unplugin.mjs", "w+", encoding="utf-8") as file:
     content = file.read()
+    print(content)
     arr = content.splitlines()
+    print("#")
+    print(arr)
     arr[6] = "import { g as getTemplates, i as icons, t as theme } from './shared/ui-pro.B6uD94pG.mjs';"
     arr[82] = "      return;"
     file.writelines(arr)
