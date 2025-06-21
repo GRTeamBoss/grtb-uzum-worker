a477c336f2034b8a8448c99499d6904c6ae3d11b
GRTeamBoss <rabbitwow97@gmail.com>
2025-06-21 09:37:11 +0500

    fix

--- overwrite.py
+++ overwrite.py
@@ -1,26 +1,23 @@
-import os
-
 def main() -> None:
-  with open("./node_modules/@nuxt/ui-pro/dist/module.mjs", "wr+", encoding="utf-8") as file:
-    content = file.read()
-    print(content)
-    arr = content.splitlines()
-    print("#")
-    print(arr)
-    arr[4] = "import { a as version, n as name, i as icons, b as addTemplates, t as theme } from './shared/ui-pro.B6uD94pG.mjs';"
-    arr[119] = "      return;"
-    file.writelines(arr)
+  file = open("./node_modules/@nuxt/ui-pro/dist/module.mjs", "r", encoding="utf-8")
+  content = file.read()
+  arr = content.splitlines()
+  arr[4] = "import { a as version, n as name, i as icons, b as addTemplates, t as theme } from './shared/ui-pro.B6uD94pG.mjs';"
+  arr[119] = "      return;"
+  file.close()
+  file = open("./node_modules/@nuxt/ui-pro/dist/module.mjs", "w", encoding="utf-8")
+  file.write("\n".join(arr))
+  file.close()
 
-
-  with open("./node_modules/@nuxt/ui-pro/dist/unplugin.mjs", "wr+", encoding="utf-8") as file:
-    content = file.read()
-    print(content)
-    arr = content.splitlines()
-    print("#")
-    print(arr)
-    arr[6] = "import { g as getTemplates, i as icons, t as theme } from './shared/ui-pro.B6uD94pG.mjs';"
-    arr[82] = "      return;"
-    file.writelines(arr)
+  file = open("./node_modules/@nuxt/ui-pro/dist/unplugin.mjs", "r", encoding="utf-8")
+  content = file.read()
+  arr = content.splitlines()
+  arr[6] = "import { g as getTemplates, i as icons, t as theme } from './shared/ui-pro.B6uD94pG.mjs';"
+  arr[82] = "      return;"
+  file.close()
+  file = open("./node_modules/@nuxt/ui-pro/dist/unplugin.mjs", "w", encoding="utf-8")
+  file.write("\n".join(arr))
+  file.close()
 
 
 if __name__ == "__main__":
